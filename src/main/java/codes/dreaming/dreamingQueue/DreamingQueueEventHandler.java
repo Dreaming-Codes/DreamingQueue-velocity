b5827a406ff0fe69db924cba9b5708db5db73fb6
DreamingCodes <me@dreaming.codes>
2025-01-26 23:48:52 +0100

    Fix bossbar progress fr this time

--- src/main/java/codes/dreaming/dreamingQueue/DreamingQueueEventHandler.java
+++ src/main/java/codes/dreaming/dreamingQueue/DreamingQueueEventHandler.java
@@ -123,7 +123,7 @@ public class DreamingQueueEventHandler {
             position++;
         }
 
-        float progress = 1 - ((float) position - 1) / (queuedPlayers.size() - 1);
+        float progress = queuedPlayers.size() == 1 ? 1 : 1 - ((float) position - 1) / (queuedPlayers.size() - 1);
 
         return BossBar.bossBar(Component.text(MessageFormat.format("Sei in coda {0}/{1}", position, queuedPlayers.size() )), progress, BossBar.Color.PURPLE, BossBar.Overlay.PROGRESS);
     }
