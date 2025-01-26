b942e8c85b72ca593625aa310ef0493d27ad4f9d
DreamingCodes <me@dreaming.codes>
2025-01-26 22:59:09 +0100

    update boss bar after transferring user

--- src/main/java/codes/dreaming/dreamingQueue/DreamingQueueEventHandler.java
+++ src/main/java/codes/dreaming/dreamingQueue/DreamingQueueEventHandler.java
@@ -204,6 +204,7 @@ public class DreamingQueueEventHandler {
                     }
                     return null;
                 });
+                this.updateBossBars();
             }
         }
     }
